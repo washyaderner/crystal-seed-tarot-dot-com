97d30933ad55beb6cbf11b9cb1327e85f93d03af
Russell Gardner <washyaderner@gmail.com>
2025-03-28 20:18:19 -0700

    [Cursor] Add 'in a consistent rotation' to Tarot classes text

--- app/page.tsx
+++ app/page.tsx
@@ -178,7 +178,7 @@ export default function Home() {
               </div>
               <div className="md:w-2/3">
                 <h3 className="text-2xl font-serif mb-4 text-white">NEW!! Tarot Classes ONLINE!!!</h3>
-                <p className="text-white mb-4">I will be running Beginner's Intermediate and Advanced Tarot courses a few times a year, so if you'd like to learn more, click below to stay up to date via my email list. I look forward to seeing you in class!</p>
+                <p className="text-white mb-4">I will be running Beginner's Intermediate and Advanced Tarot courses in a consistent rotation a few times a year, so if you'd like to learn more, click below to stay up to date via my email list. I look forward to seeing you in class!</p>
                 <div className="text-center md:text-left mt-6">
                   <Button asChild variant="outline">
                     <Link href="/services#tarot-classes" className="text-white">
